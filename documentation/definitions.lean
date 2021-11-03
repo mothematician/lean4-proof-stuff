7d927eff7befc5360a9e429011044ccd87647f7b
mon-neutrino <monica.sing.2023@gmail.com>
2021-11-03 14:56:22 +0700

    variables & sections done

--- documentation/definitions.lean
+++ documentation/definitions.lean
@@ -50,4 +50,70 @@ def double4times (x : Nat) := double4 (double4 x)
 -- LOCAL DEFINITIONS
 
 -- introduce local definitions using `let` keyword
+  -- ie, `let a := t1; t2` equivalent to replacing every occurence of a in t2 by t1
+
+  #check let y := 2 + 2; y * y   -- Nat
+  #eval  let y := 2 + 2; y * y   -- 16
+
+  def twice_double (x : Nat) : Nat :=
+    let y := x + x; y * y
+
+  #eval twice_double 2   -- 16
+
+
+-- `variables and sections`
+
+  -- declare `variable` names and their types
+    -- instructs lean to insert decalred varaibles as bound variables in defs that refer to them by name
+    variable (α β γ : Type)
+    variable (g : β → γ) (f : α → β) (h : α → α)
+    variable (x : α)
+
+    def compose1 := g (f x)
+    def doTwice := h (h x)
+    def doThrice := h (h (h x))
+
+    #print compose1
+    #print doTwice
+    #print doThrice
+
+  -- `section`, `end`
+    -- for limiting scope of variable to specific sections only
+    
+    section useful
+      variable (α β γ : Type)
+      variable (g : β → γ) (f : α → β) (h : α → α)
+      variable (x : α)
+
+      def compose2 := g (f x)
+      def doTwice2 := h (h x)
+      def doThrice2 := h (h (h x))
+    end useful
+    
+    -- after `end` the variable name no longer has the values defined in scope
+    -- can also be nested. ie:
+    
+      section phat
+      variable (α β γ : Type)
+
+        section phat1
+          variable (g : β → γ) (f : α → β) (h : α → α)
+          variable (x : α)
+
+          def compose3 := g (f x)
+          def doTwice3 := h (h x)
+          def doThrice3 := h (h (h x))
+        end phat1
+
+        section phat2
+          variable (g : β → γ) (f : α → β) (h : γ → α)(i : γ → β)
+          variable (x : γ)
+
+          def compose4 := f (h x)
+          def doTwice4 := h (g (i x))
+          def doThrice4 := g (f (h x))
+        end phat2
+
+      end phat
+
 
